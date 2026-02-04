64be35b11cb613444b461aaf0ca43f382abfdb4d
sirat <you@example.com>
2026-02-04 12:02:21 +0530

    ggg

--- CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
+++ CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
@@ -13,13 +13,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
 
 @RestController
-@CrossOrigin(origins = "*")
+@RequestMapping("/api/customer")
+@CrossOrigin(origins = "http://localhost:8080")
 
 public class CustomerController {	
 	
