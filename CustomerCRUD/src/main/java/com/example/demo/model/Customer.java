260219a464cdf504410d7a51c8758625d4df4b1a
Sirat Shaikh <you@example.com>
2026-02-05 16:15:27 +0530

    id auto

--- CustomerCRUD/src/main/java/com/example/demo/model/Customer.java
+++ CustomerCRUD/src/main/java/com/example/demo/model/Customer.java
@@ -1,6 +1,8 @@
 package com.example.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
@@ -8,6 +10,7 @@ import jakarta.persistence.Id;
 public class Customer {
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Integer id;
 	private String name;
 	private String mob;
