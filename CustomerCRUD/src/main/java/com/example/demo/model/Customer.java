6cf580de2eaaed43ce4bec56db96d0b68bcede1b
Sirat Shaikh <you@example.com>
2026-02-05 16:41:25 +0530

    auto to identity

--- CustomerCRUD/src/main/java/com/example/demo/model/Customer.java
+++ CustomerCRUD/src/main/java/com/example/demo/model/Customer.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 public class Customer {
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
 	private String mob;
