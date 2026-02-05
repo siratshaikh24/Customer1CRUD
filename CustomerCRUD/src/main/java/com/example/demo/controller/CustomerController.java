5a1a2377c9549d904a1e393907c5e06bb045b974
Sirat Shaikh <you@example.com>
2026-02-05 15:01:27 +0530

    code updated

--- CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
+++ CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,85 +1,78 @@
 package com.example.demo.controller;
 
-
 import java.util.List;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
 
 @RestController
-@CrossOrigin(origins = "*")
+@RequestMapping("/")
+public class CustomerController {
 
-public class CustomerController {	
-	
-	
-	// Customer1
-	//Customer
-	
-	@Autowired
-	private CustomerService cs;
-	@GetMapping("/")
-	public String getMethodName() {
-		return "I am Sirat";
-		
-		
-	}
-	
-	@PostMapping("add")
-	public void add(@RequestBody Customer c) {		
-		cs.add(c);
-	}
-	
-	
-	@PostMapping("add all")
-	public void addAll(@RequestBody List<Customer> list) {		
-		cs.addAll(list);
-		
-				//fhjsjkjjfk
-	}
-		
+    @Autowired
+    private CustomerService cs;
 
-	@GetMapping("/display")
-	public List<Customer> display() {
-		return cs.display();
-		//hffjfjfj
-		//hdjhjh
-	}
-	
-		@DeleteMapping("delete/{id}")
-		public ResponseEntity<?> delete(@PathVariable Integer id) {
-			Customer temp = cs.delete(id);
-			if(temp == null) {
-				return ResponseEntity.status(HttpStatus.NOT_FOUND)
-						.body("Not Found !!");	
-			}
-		return ResponseEntity.ok(temp);		
-		
-		}	
-		
-		@PostMapping("search/{id}") 
-			public Customer search(@PathVariable Integer id) {
-				
-				return cs.search(id);				
-		}
-		
-		@PostMapping("search/mob/{mob}") 
-		public Customer search(@PathVariable String mob) {
-			
-			return cs.findBymob(mob);			
-	
-		}
+    // Welcome API
+    @GetMapping
+    public String welcome() {
+        return "I am Sirat";
+    }
 
+    // Add single customer
+    @PostMapping("/add")
+    public ResponseEntity<String> add(@RequestBody Customer c) {
+        cs.add(c);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Customer added");
+    }
+
+    // Add multiple customers
+    @PostMapping("/add-all")
+    public ResponseEntity<String> addAll(@RequestBody List<Customer> list) {
+        cs.addAll(list);
+        return ResponseEntity.status(HttpStatus.CREATED).body("All customers added");
+    }
+
+    // Display all customers
+    @GetMapping("/display")
+    public List<Customer> display() {
+        return cs.display();
+    }
+
+    // Delete customer by id
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        Customer temp = cs.delete(id);
+        if (temp == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Customer not found");
+        }
+        return ResponseEntity.ok(temp);
+    }
+
+    // Search by id
+    @GetMapping("/search/id/{id}")
+    public ResponseEntity<?> searchById(@PathVariable Integer id) {
+        Customer c = cs.search(id);
+        if (c == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Customer not found");
+        }
+        return ResponseEntity.ok(c);
+    }
+
+    // Search by mobile number
+    @GetMapping("/search/mob/{mob}")
+    public ResponseEntity<?> searchByMob(@PathVariable String mob) {
+        Customer c = cs.findBymob(mob);
+        if (c == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body("Customer not found");
+        }
+        return ResponseEntity.ok(c);
+    }
 }
-		
