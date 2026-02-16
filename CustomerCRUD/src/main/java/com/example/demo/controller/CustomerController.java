7529592f03453d1f333b4acc5b124c5828899478
sirat <you@example.com>
2026-02-16 17:09:50 +0530

    Hiiii

--- CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
+++ CustomerCRUD/src/main/java/com/example/demo/controller/CustomerController.java
@@ -11,8 +11,7 @@ import com.example.demo.model.Customer;
 import com.example.demo.service.CustomerService;
 
 @RestController
-@CrossOrigin(methods = {RequestMethod.GET,RequestMethod.POST}, origins =  {"*"})
-
+@CrossOrigin("*")
 public class CustomerController {
 
     @Autowired
